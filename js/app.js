f9eed70a024652a313be2c4c8cb83880924acf9a
Saykot Kumar Barmon <saykot.cse43@gmail.com>
2022-03-10 22:08:45 +0600

    liked button active

--- js/app.js
+++ js/app.js
@@ -16,7 +16,7 @@ const isLiked = (id) => {
 };
 
 const addToLiked = (id) => {
-    likedPostsId.plus(id); 
+    likedPostsId.push(id); 
     showPosts(posts);
 };
 
@@ -120,7 +120,7 @@ const createPost = (post) => {
                   <div class="post__description">
                     <small>
                       <a class="post__name--underline" href="#">
-                          ${post.comments?.user}
+                          ${post.comments ?.user}
                       </a>
                       ${post.comments?.text}
                     </small>
